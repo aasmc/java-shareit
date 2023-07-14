6a6df182a9d6e670edc38b8e5aa8476aedb32e22
aleksas <test@test.com>
2023-07-14 23:17:48 +0300

    fix: checkstyle unused import

--- src/test/java/ru/practicum/shareit/user/controller/UserControllerTest.java
+++ src/test/java/ru/practicum/shareit/user/controller/UserControllerTest.java
@@ -7,7 +7,6 @@ import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
 
-import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
